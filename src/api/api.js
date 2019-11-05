8a4dacab03335d10ec45d4761af902c4e4a34f26
suyongzhi <1125699801@qq.com>
2019-11-05 15:41:23 +0800

    修改用户中心收货地址功能接口

--- src/api/api.js
+++ src/api/api.js
@@ -93,13 +93,13 @@ export const addMessage = params => {return axios.post(`${local_host}/messages/`
 export const delMessages = messageId => {return axios.delete(`${local_host}/messages/`+messageId+'/')}
 
 //添加收货地址
-export const addAddress = params => {return axios.post(`${host}/address/`, params)}
+export const addAddress = params => {return axios.post(`${local_host}/address/`, params)}
 
 //删除收货地址
-export const delAddress = addressId => {return axios.delete(`${host}/address/`+addressId+'/')}
+export const delAddress = addressId => {return axios.delete(`${local_host}/address/`+addressId+'/')}
 
 //修改收货地址
-export const updateAddress = (addressId, params) => {return axios.patch(`${host}/address/`+addressId+'/', params)}
+export const updateAddress = (addressId, params) => {return axios.patch(`${local_host}/address/`+addressId+'/', params)}
 
 //获取收货地址
-export const getAddress = () => {return axios.get(`${host}/address/`)}
+export const getAddress = () => {return axios.get(`${local_host}/address/`)}
